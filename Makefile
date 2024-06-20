36ac9bbe6a60f162ddc80a38336c6cd95c9b6966
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2024-06-20 19:20:23 +0300

    :bulb: Fix: updated home.nix

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
 .PHONY: update
 update:
 	home-manager switch --flake .#ikostov2
+
+.PHONY: clean
+clean:
+	nix-collect-garbage -d
--- home.nix
+++ home.nix
@@ -2,11 +2,11 @@
 
 {
   home = {
-    isNormalUser = true;
+    # isNormalUser = true;
     homeDirectory = "/home/ikostov2";
     username = "ikostov2";
-    description = "ikostov2";
-    extraGroups = [ "libvirtd" "adbusers" "kvm" "docker" "users" "networkmanager" "wheel" ];
+    # description = "ikostov2";
+    # users.users.ikostov2.extraGroups = [ "libvirtd" "adbusers" "kvm" "docker" "users" "networkmanager" "wheel" ];
     packages = with pkgs; [
       postman
       obs-studio
@@ -61,7 +61,19 @@
       i3
     ];
   };
-
+  # Services
+  # services = {
+  #   openssh.enable = true;
+  #   libinput.enable = true;
+  #   openvpn.servers = {
+  #     personalVPN = {
+  #       config = "config /home/ikostov2/.config/openvpn/personalVPN.ovpn";
+  #       updateResolvConf = true;
+  #       autoStart = true;
+  #     };
+  #   };
+  # };
+  #
   # Programs
   programs = {
     firefox.enable = true;
@@ -75,66 +87,54 @@
       defaultEditor = true;
       viAlias = true;
       vimAlias = true;
-    };
-    # .bashrc
-    bash = {
-      shellAliases = {
-        clip = "xclip -selection clipboard";
-        timmy = "tmux new-session '~/.local/bin/tmux-ls-sessionizer'";
-        buzz = "cd $(find . -type d | fzf)";
-        git-all = "git add . && git commit && ( git push || git push --set-upstream origin master )";
-        py-setup-venv = "~/.local/bin/python/python-venv-setup";
+      # .bashrc
+      bash = {
+        shellAliases = {
+          clip = "xclip -selection clipboard";
+          timmy = "tmux new-session '~/.local/bin/tmux-ls-sessionizer'";
+          buzz = "cd $(find . -type d | fzf)";
+          git-all = "git add . && git commit && ( git push || git push --set-upstream origin master )";
+          py-setup-venv = "~/.local/bin/python/python-venv-setup";
+        };
       };
     };
   };
-
   # Environment
-  environment = {
+  home = {
     # Needed for compatibility purposes
     sessionVariables = {
       PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
     };
     # Env vars
-    variables = {
-      EDITOR = "nvim";
-      JAVA_HOME = "${pkgs.jdk}/lib/openjdk";
-      PATH = "${pkgs.jdk}/bin:" + builtins.getEnv "PATH";
-    };
+    # variables = {
+    #   EDITOR = "nvim";
+    #   JAVA_HOME = "${pkgs.jdk}/lib/openjdk";
+    #   PATH = "${pkgs.jdk}/bin:" + builtins.getEnv "PATH";
+    # };
   };
 
   # Quemu and Docker
-  virtualisation = {
-    libvirtd.enable = true;
-    spiceUSBRedirection.enable = true;
-    docker = {
-      enable = true;
-      rootless = {
-        enable = true;
-        setSocketVariable = true;
-      };
-
-    };
-  };
-
-  # Services
-  services = {
-    openssh.enable = true;
-    libinput.enable = true;
-    openvpn.servers = {
-      personalVPN = {
-        config = "config /home/ikostov2/.config/openvpn/personalVPN.ovpn";
-        updateResolvConf = true;
-        autoStart = true;
-      };
-    };
-  };
+  # virtualisation = {
+  #   libvirtd.enable = true;
+  #   spiceUSBRedirection.enable = true;
+  #   docker = {
+  #     enable = true;
+  #     rootless = {
+  #       enable = true;
+  #       setSocketVariable = true;
+  #     };
+  #
+  #   };
+  # };
+  #
   # Enable nerd fonts
-  fonts.packages = with pkgs; [
-    (nerdfonts.override { fonts = [ "0xProto" ]; })
-  ];
+  # fonts.packages = with pkgs; [
+  #    (nerdfonts.override { fonts = [ "0xProto" ]; })
+  #  ];
 
 
   home.stateVersion = "24.05";
   # Let home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
+
