765f42a7b5375e98783ecdc5e7fcc124ba2a3a6f
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2024-06-30 19:34:23 +0300

    ✨ Feat: enabled connman option

--- hosts/shared/options/default.nix
+++ hosts/shared/options/default.nix
@@ -11,6 +11,7 @@
       ./i3
       ./neovim
       ./nerdfonts
+      ./network
       ./openvpn
       ./users
       ./virtualisation
---
+++ hosts/shared/options/network/default.nix
@@ -0,0 +1,4 @@
+{ config, lib, pkgs, ... }:
+{
+  programs.connman.enable = true;
+}
