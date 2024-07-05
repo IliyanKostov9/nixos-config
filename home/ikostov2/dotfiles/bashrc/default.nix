9d0c62c02a00ad7ea18f4e164fa6d031e631a888
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2024-07-05 20:56:43 +0300

    🧑‍💻 Feat: added shellAlias to quickly jump to the git root project

--- home/ikostov2/dotfiles/bashrc/default.nix
+++ home/ikostov2/dotfiles/bashrc/default.nix
@@ -12,9 +12,9 @@
     shellAliases = {
       nvim-su = "sudo -E nvim .";
       clip = "xclip -selection clipboard";
-      timmy = "tmux new-session '~/.local/bin/tmux-ls-sessionizer'";
       buzz = "cd $(find . -type d | fzf)";
       git-all = "git add . && git commit && ( git push || git push --set-upstream origin master )";
+      git-root = "cd $( git rev-parse --show-toplevel )";
       py-setup-venv = "~/.local/bin/python/python-venv-setup";
       mvn-spring = "mvn spring-boot:run";
       mvn-deps = "mvn dependency:resolve";
