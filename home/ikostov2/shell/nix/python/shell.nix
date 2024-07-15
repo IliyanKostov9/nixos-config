30614dc19ecad9041392962568ce107e7c885c2d
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2024-07-15 22:28:09 +0300

    :sparkles: Feat: added nix specific shells

---
+++ home/ikostov2/shell/nix/python/shell.nix
@@ -0,0 +1,30 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+(pkgs.buildFHSUserEnv {
+  name = "pipzone";
+  targetPkgs = pkgs: (with pkgs; [
+    python311
+    poetry
+    python311Packages.virtualenv
+    zsh
+    opencv4
+    mesa
+    libGL
+    libGLU
+    xorg.libX11
+    glib
+    glibc
+    stdenv.cc.cc.lib
+    libsForQt5.qt5.qtbase
+    libsForQt5.qt5ct
+    zlib
+  ]);
+  runScript = "zsh";
+
+
+  shellHook = ''
+    export LD_LIBRARY_PATH="${pkgs.zlib}/lib"
+    export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath}:$LD_LIBRARY_PATH"
+    export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib.outPath}/lib:$LD_LIBRARY_PATH"
+  '';
+}).env
