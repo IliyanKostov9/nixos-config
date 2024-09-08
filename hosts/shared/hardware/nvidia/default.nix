0e7c7c5ed2af1ac7f96730141a4b62aa82e1908a
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2024-09-08 20:06:45 +0300

    :coffin: chore: removed unused code

--- hosts/shared/hardware/nvidia/default.nix
+++ hosts/shared/hardware/nvidia/default.nix
@@ -12,6 +12,7 @@ in
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = false; # Disabled, because vulkan.h breaks it. Affected versions: 558 >
+    # package = config.boot.kernelPackages.nvidiaPackages.stable;
     package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
       inherit version;
       sha256_64bit = "sha256-8pMskvrdQ8WyNBvkU/xPc/CtcYXCa7ekP73oGuKfH+M=";
--- hosts/shared/hardware/nvidia/overlay.nix
+++
@@ -1,5 +0,0 @@
-self: super: {
-  nvidiaSettings = super.nvidiaSettings.overrideAttrs (oldAttrs: {
-    buildInputs = oldAttrs.buildInputs ++ [ self.vulkan-headers ];
-  });
-}
