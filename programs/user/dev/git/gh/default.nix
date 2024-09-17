5fdd08455af3cda7b7e6c9ad5bc7eddefc656756
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2024-09-17 21:46:32 +0300

    :arrow_up: feat: added tf, awscli and gh to unstable channel

--- programs/user/dev/git/gh/default.nix
+++ programs/user/dev/git/gh/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, config, ... }:
+{ pkgs_unstable, lib, config, ... }:
 with lib;
 let cfg = config.modules.gh;
 in
@@ -7,7 +7,7 @@ in
 
   config = mkIf cfg.enable {
     home.packages = [
-      pkgs.gh
+      pkgs_unstable.gh
     ];
   };
 
