9924c217d616b8fe06bb9066ce5ebf503cdd9256
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2024-10-10 10:52:52 +0300

    :bulb: chore: commented fzf-file-search

--- programs/user/dev/shell/sh/alias/default.nix
+++ programs/user/dev/shell/sh/alias/default.nix
@@ -7,24 +7,24 @@ let
       text = builtins.readFile ../bin/bash/eza/tree-icons.sh;
     };
 
-  fzf-file-search = pkgs.writeShellApplication {
-    name = "fzf-file-search";
-    runtimeInputs = [ pkgs.fzf pkgs.bat pkgs.tree ];
-
-    text = ''
-      selection=$(find . -type f -o -type d | fzf --cycle --border=thinblock --border-label='| Search here |' --preview 'bat --color=always --style=numbers --theme=base16-256 --line-range=:500 {} || tree -C {}' --preview-label='Preview')
-
-      if [ -d "$selection" ]; then
-        "$selection" 
-      else
-         "$(dirname "$selection")"
-      fi
-    '';
-  };
+  # fzf-file-search = pkgs.writeShellApplication {
+  #   name = "fzf-file-search";
+  #   runtimeInputs = [ pkgs.fzf pkgs.bat pkgs.tree ];
+  #
+  #   text = ''
+  #     selection=$(find . -type f -o -type d | fzf --cycle --border=thinblock --border-label='| Search here |' --preview 'bat --color=always --style=numbers --theme=base16-256 --line-range=:500 {} || tree -C {}' --preview-label='Preview')
+  #
+  #     if [ -d "$selection" ]; then
+  #       "$selection" 
+  #     else
+  #        "$(dirname "$selection")"
+  #     fi
+  #   '';
+  # };
 in
 {
   home.packages = lib.optionals (config.modules.zsh.enable || config.modules.bash.enable) [
     lst
-    fzf-file-search
+    # fzf-file-search
   ];
 }
