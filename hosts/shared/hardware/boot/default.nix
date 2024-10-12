a245c46bd36ef40574b37c687f50bdc0d15fa50e
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2024-10-12 18:42:20 +0300

    fix: boot

--- hosts/shared/hardware/boot/default.nix
+++ hosts/shared/hardware/boot/default.nix
@@ -1,17 +1,21 @@
 { lib, pkgs, modulesPath, host_attr, ... }:
 
-with host_attr; {
-
+with host_attr;
+let
+  is-secure-boot-enabled = builtins.pathExists "/etc/secureboot";
+in
+{
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
   boot = boot // {
     kernelPackages = pkgs.linuxPackages_6_11;
     loader = {
-      systemd-boot.enable = if builtints.pathExists "/etc/secureboot" then false else true;
+      # Note: is-secure-boot-enabled value is reverted, since system boot expects to be true when secure boot is disabled and vice versa
+      systemd-boot.enable = !is-secure-boot-enabled;
       efi.canTouchEfiVariables = true;
     };
-  } // (if builtins.pathExists "/etc/secureboot" then {
+  } // (if is-secure-boot-enabled then {
     # NEEDED: For Garnix
     lanzaboote = {
       enable = true;
