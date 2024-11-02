2a4ab5afda7bf74eceabcb0bbef075e2674f8f89
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2024-11-02 18:37:07 +0200

    feat: added 165 refresh rate for personal PC

--- hosts/personal/desktop/hardware/displayManager/default.nix
+++ hosts/personal/desktop/hardware/displayManager/default.nix
@@ -4,4 +4,7 @@
   # Enable automatic login for the user.
   services.displayManager.autoLogin.enable = true;
   services.displayManager.autoLogin.user = host_attr.auto-login-user;
+  services.xserver.screenSection = ''
+    Option "metamodes" "DP-2: 1920x1080_165 +0+0 {AllowGSYNCCompatible=On}"
+  '';
 }
