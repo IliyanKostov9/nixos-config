a649fcb275c40b94c791882a349a16bb70650cdc
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2024-11-03 11:14:37 +0200

    feat: added label for the HDD

--- hosts/personal/desktop/hardware/mount/default.nix
+++ hosts/personal/desktop/hardware/mount/default.nix
@@ -1,7 +1,7 @@
 { ... }:
 {
   fileSystems."/mnt/HDD" = {
-    device = "/dev/sdb1";
+    device = "/dev/disk/by-label/NIXOS_HDD";
     fsType = "ntfs";
     options = [ "users" "nofail" "rw" ];
   };
