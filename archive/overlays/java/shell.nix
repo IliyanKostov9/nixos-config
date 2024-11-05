be14eb6a6ca852169491fa9f6b5a75417c1ec2fe
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2024-11-05 22:21:50 +0200

    feat: added shell for the java overlay

---
+++ archive/overlays/java/shell.nix
@@ -0,0 +1,31 @@
+{ pkgs ? import <nixpkgs> { } }:
+let
+  username = "ikostov2";
+
+  jdk-overlay = self: super: {
+    jdk =
+      if builtins.pathExists "/home/${username}/.config/certs" then
+        super.jdk8.override
+          {
+            cacert = self.runCommand "CC Maven" { } ''
+              mkdir -p $out/etc/ssl/certs
+              cat ${super.cacert}/etc/ssl/certs/ca-bundle.crt \
+                ${/home/${username}/.config/certs/cc_root.cer} > $out/etc/ssl/certs/ca-bundle.crt
+            '';
+          }
+      else super.jdk8;
+  };
+
+  customPkgs = import <nixpkgs> {
+    overlays = [ jdk-overlay ];
+  };
+in
+pkgs.mkShell {
+  name = "java8";
+  buildInputs = with pkgs; [
+    customPkgs.jdk
+  ];
+
+  runScript = "zsh";
+  shellHook = "${pkgs.zsh}/bin/zsh";
+}
