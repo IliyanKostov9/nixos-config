a5467a34d081ce0308477f52abbfd1e635043bf9
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2024-11-17 17:09:58 +0200

    fix: garnix

--- flakes/export-image.nix
+++ flakes/export-image.nix
@@ -11,12 +11,16 @@ rec {
       nixos-generators.nixosGenerate
         {
           system = "x86_64-linux";
-          modules = host_attr.modules ++ [ nix-index-database.nixosModules.nix-index ];
+          modules = host_attr.modules ++ [
+            nix-index-database.nixosModules.nix-index
+            sops-nix.nixosModules.sops
+            lanzaboote.nixosModules.lanzaboote
+          ];
           format = "iso";
           specialArgs = { inherit host_name host_attr; inherit (shared) pkgs system stateVersion users; };
         }
     )
     shared.config_system.hosts;
 
-  flake.defaultPackage.x86_64-linux = builtins.head (builtins.attrValues flake.packages.x86_64-linux or { });
+  flake.defaultPackage.x86_64-linux = builtins.head (builtins.attrValues flake.packages.x86_64-linux);
 }
