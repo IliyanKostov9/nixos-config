38e21bd457b7094ad67d571d6279c1ccb0225cae
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2024-11-20 13:30:20 +0200

    chore: resolved github actions errors

--- programs/user/cloud/azure-cli/default.nix
+++ programs/user/cloud/azure-cli/default.nix
@@ -11,10 +11,6 @@ in
   options.modules.azure-cli = { enable = mkEnableOption "azure-cli"; };
 
   config = mkIf cfg.enable (
-    let
-      # inherit (builtins) filter attrValues;
-      # azure-cli = pkgs.azure-cli.withExtensions (filter lib.isDerivation (attrValues pkgs.azure-cli-extensions));
-    in
     {
       home.packages = with pkgs; [ azure-cli ];
     }
