2eee9e1b95cebf014093c9246a8e1446727f600d
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2024-11-20 20:47:13 +0200

    fix: redshift

--- programs/user/preferences/red-light/default.nix
+++ programs/user/preferences/red-light/default.nix
@@ -8,27 +8,30 @@ in
   config = mkIf cfg.enable {
     home.packages = with pkgs; [ redshift ];
 
-    services.redshift.settings = {
-      redshift = {
-        temp-day = 6500;
-        temp-night = 3000;
-        transition = 1;
-        gamma = "0.8:0.7:0.8";
-        location-provider = "manual";
-        adjustment-method = "randr";
-      };
-      manual = {
-        lat = 43.216640;
-        lon = 27.911810;
-      };
+    services.redshift = {
+      enable = true;
+      settings = {
+        redshift = {
+          temp-day = lib.mkForce 6500;
+          temp-night = lib.mkForce 3000;
+          transition = 1;
+          gamma = "0.8:0.7:0.8";
+          location-provider = "manual";
+          adjustment-method = "randr";
+        };
+        manual = {
+          lat = 43.216640;
+          lon = 27.911810;
+        };
 
-      randr = {
-        screen = 0;
-      };
+        randr = {
+          screen = 0;
+        };
 
-      period = {
-        night = "21:00-7:00";
-        day = "7:00-21:00";
+        period = {
+          night = "21:00-7:00";
+          day = "7:00-21:00";
+        };
       };
     };
   };
