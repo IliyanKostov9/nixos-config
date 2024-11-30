1fe3688b2a824ac01a80b19d5457dd0804d5daf9
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2024-11-30 19:37:44 +0200

    fix: librewolf profiles

---
+++ programs/user/browsers/librewolf/about-config/settings/default.nix
@@ -0,0 +1,42 @@
+{
+  # Enable vertical tabs
+  "sidebar.verticalTabs" = true;
+  "sidebar.revamp" = true;
+  "sidebar.visibility" = "always-show";
+  "sidebar.main.tools" = "aichat,history";
+  "sidebar.position_start" = true;
+
+  "identity.fxaccounts.enabled " = true;
+  "browser.tabs.hoverPreview.showThumbnails " = false;
+  "browser.contentblocking.category" = "strict";
+  "browser.download.panel.shown" = true;
+  "middlemouse.paste" = false;
+  "general.autoScroll" = true;
+  "browser.tabs.warnOnClose" = true;
+  "browser.toolbars.bookmarks.visibility" = "newtab";
+  "browser.sessionstore.restore_pinned_tabs_on_demand" = true;
+  "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+
+  "browser.urlbar.suggest.bookmark" = true;
+  "browser.urlbar.suggest.engines" = true;
+  "browser.urlbar.suggest.history" = true;
+  "browser.urlbar.suggest.openpage" = true;
+  "browser.urlbar.suggest.topsites" = true;
+
+  # Disable annoying firefox builtin plugins
+  "extensions.pocket.enabled" = false;
+  "browser.tabs.tabmanager.enabled" = false;
+  "browser.firefox-view.virtual-list.enabled" = false;
+  "browser.urlbar.suggest.pocket" = false;
+
+  "extensions.pocket.showHome" = true;
+  "extensions.autoDisableScopes" = 0;
+  # Privacy settings
+  "webgl.disabled" = false;
+  "privacy.resistRingerprinting" = false;
+  "privacy.clearOnShutdown.history" = false;
+  "privacy.clearOnShutdown.downloads" = false;
+  "privacy.clearOnShutdown.cookies" = false;
+  "network.cookie.lifetimePolicy" = 0;
+  "geo.enabled" = false;
+}
