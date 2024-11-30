21e2b73e669776de087f9e84621dfcd88cd79f0e
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2024-11-30 15:30:05 +0200

    fix: librewolf profiles

--- programs/user/browsers/librewolf/default.nix
+++ programs/user/browsers/librewolf/default.nix
@@ -5,7 +5,48 @@ let
   inherit (config.sops) secrets;
 
   cfg = config.modules.browsers.librewolf;
-  settings = (import ./settings.nix { }).settings;
+  settings = {
+    # Enable vertical tabs
+    "sidebar.verticalTabs" = true;
+    "sidebar.revamp" = true;
+    "sidebar.visibility" = "always-show";
+    "sidebar.main.tools" = "aichat,history";
+    "sidebar.position_start" = true;
+
+    "identity.fxaccounts.enabled " = true;
+    "browser.tabs.hoverPreview.showThumbnails " = false;
+    "browser.contentblocking.category" = "strict";
+    "browser.download.panel.shown" = true;
+    "middlemouse.paste" = false;
+    "general.autoScroll" = true;
+    "browser.tabs.warnOnClose" = true;
+    "browser.toolbars.bookmarks.visibility" = "newtab";
+    "browser.sessionstore.restore_pinned_tabs_on_demand" = true;
+    "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+
+    "browser.urlbar.suggest.bookmark" = true;
+    "browser.urlbar.suggest.engines" = true;
+    "browser.urlbar.suggest.history" = true;
+    "browser.urlbar.suggest.openpage" = true;
+    "browser.urlbar.suggest.topsites" = true;
+
+    # Disable annoying firefox builtin plugins
+    "extensions.pocket.enabled" = false;
+    "browser.tabs.tabmanager.enabled" = false;
+    "browser.firefox-view.virtual-list.enabled" = false;
+    "browser.urlbar.suggest.pocket" = false;
+
+    "extensions.pocket.showHome" = true;
+    "extensions.autoDisableScopes" = 0;
+    # Privacy settings
+    "webgl.disabled" = false;
+    "privacy.resistRingerprinting" = false;
+    "privacy.clearOnShutdown.history" = false;
+    "privacy.clearOnShutdown.downloads" = false;
+    "privacy.clearOnShutdown.cookies" = false;
+    "network.cookie.lifetimePolicy" = 0;
+    "geo.enabled" = false;
+  };
   extensions = with pkgs.nur.repos.rycee.firefox-addons; [
     ublock-origin
     privacy-badger
@@ -48,12 +89,8 @@ in
       # package = pkgs.librewolf-bin;
 
       profiles = {
-        default = {
-          id = 0;
-          inherit extensions settings search;
-        };
         Main = {
-          id = 1;
+          id = 0;
           name = "Main";
           containersForce = true;
           isDefault = true;
@@ -61,7 +98,7 @@ in
             Github = {
               id = 0;
               name = "Github";
-              color = "white";
+              color = "toolbar";
               icon = "fingerprint";
             };
 
@@ -82,14 +119,14 @@ in
             Microsoft = {
               id = 3;
               name = "Microsoft";
-              color = "blue";
+              color = "turquoise";
               icon = "fingerprint";
             };
 
             AWS = {
               id = 4;
               name = "AWS";
-              color = "yello";
+              color = "yellow";
               icon = "fingerprint";
             };
 
@@ -100,27 +137,27 @@ in
               icon = "food";
             };
           };
-          inherit settings search;
+          inherit settings search extensions;
         };
         Youtube = {
-          id = 2;
+          id = 1;
           name = "Youtube";
-          inherit settings search;
+          inherit settings search extensions;
         };
         Linked-In = {
-          id = 3;
+          id = 2;
           name = "Linked-In";
-          inherit settings search;
+          inherit settings search extensions;
         };
         Work = {
-          id = 4;
+          id = 3;
           name = builtins.readFile secrets.work_name.path;
-          inherit settings search;
+          inherit settings search extensions;
         };
         Work_Project1 = {
-          id = 5;
+          id = 4;
           name = builtins.readFile secrets.work_project1_name.path;
-          inherit settings search;
+          inherit settings search extensions;
         };
       };
     };
