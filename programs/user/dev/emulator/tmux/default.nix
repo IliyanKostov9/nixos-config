a676cf96d7dfc52d5aa18cf4818fca18ca84aebb
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2024-12-04 15:42:48 +0200

    fix: tmux.conf

--- programs/user/dev/emulator/tmux/default.nix
+++ programs/user/dev/emulator/tmux/default.nix
@@ -1,6 +1,8 @@
 { pkgs, lib, config, ... }:
 with lib;
-let cfg = config.modules.dev.emulator.tmux;
+let
+  cfg = config.modules.dev.emulator.tmux;
+  tmux-conf = builtins.replaceStrings [ "$Work*" "$Work_Project1*" ] [ config.globals.work_name config.globals.work_project1_name ] (lib.fileContents ./tmux.conf);
 in
 {
   options.modules.dev.emulator.tmux = { enable = mkEnableOption "tmux"; };
@@ -18,9 +20,7 @@ in
         yank
         open
       ];
-      # extraConfig = import (./tmux.conf).tmux-conf;
-      extraConfig = lib.fileContents ./tmux.conf;
-      disableConfirmationPrompt = false;
+      extraConfig = tmux-conf;
       clock24 = true;
       baseIndex = 1;
       mouse = true;
