a13399e514840153be7b2ee762d51f7f5006a5f5
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2024-12-20 11:28:12 +0200

    fix: librewolf profiles

--- home/ikostov2/options/librewolf/profiles/default.nix
+++ home/ikostov2/options/librewolf/profiles/default.nix
@@ -1,7 +1,6 @@
 { lib, config, pkgs, work_name, work_project1_name }:
-let
-  plugins = if lib.hasAttr "nur" pkgs then pkgs.nur.repos.rycee.firefox-addons else lib.warn "> Firefox addons are not installed! Please install nur repository to add them!" { };
-in
+with pkgs.nur.repos.rycee.firefox-addons;
+
 {
   Main = {
     id = 0;
@@ -9,14 +8,14 @@ in
     containersForce = true;
     isDefault = true;
     containers = import ./containers/Main;
-    extensions = lib.mkIf (lib.hasAttr "firefox-addons" plugins) (with plugins;[
+    extensions = [
       passbolt
       ublock-origin
       privacy-badger
       darkreader
       i-dont-care-about-cookies
       user-agent-string-switcher
-    ]);
+    ];
   };
   Youtube = {
     id = 1;
@@ -24,26 +23,26 @@ in
     containersForce = false;
     isDefault = false;
     containers = import ./containers/Youtube;
-    extensions = lib.mkIf (lib.hasAttr "firefox-addons" plugins) (with plugins;[
+    extensions = [
       ublock-origin
       privacy-badger
       darkreader
       i-dont-care-about-cookies
       user-agent-string-switcher
-    ]);
+    ];
   };
   Linked-In = {
     id = 2;
     name = "Linked-In";
     containersForce = false;
     isDefault = false;
-    extensions = lib.mkIf (lib.hasAttr "firefox-addons" plugins) (with plugins;[
+    extensions = [
       ublock-origin
       privacy-badger
       darkreader
       i-dont-care-about-cookies
       user-agent-string-switcher
-    ]);
+    ];
   };
 
   Work = {
@@ -51,14 +50,14 @@ in
     name = work_name;
     containersForce = false;
     isDefault = false;
-    extensions = lib.mkIf (lib.hasAttr "firefox-addons" plugins) (with plugins;[
+    extensions = [
       passbolt
       ublock-origin
       privacy-badger
       darkreader
       i-dont-care-about-cookies
       user-agent-string-switcher
-    ]);
+    ];
   };
 
   Work_Project1 = {
@@ -66,12 +65,12 @@ in
     name = work_project1_name;
     containersForce = false;
     isDefault = false;
-    extensions = lib.mkIf (lib.hasAttr "firefox-addons" plugins) (with plugins;[
+    extensions = [
       ublock-origin
       privacy-badger
       darkreader
       i-dont-care-about-cookies
       user-agent-string-switcher
-    ]);
+    ];
   };
 }
