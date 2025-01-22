4ab824ddc2ef331c9c04e7e33a32a4dfd39d7c56
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2025-01-22 16:45:33 +0000

    feat/garbage-collection (#23)
    # Description
    
    - feat: garbage collection
    
    ## Checklist
    
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my own code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [ ] My changes generate no new warnings
    - [ ] I have added manual/automated tests that prove my fix is effective or that my feature works
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] Any dependent changes have been merged and published in downstream modules
    
    ## Remark
    
    For automation please see [closing-issues-using-keywords][closing_issues_using_keywords]
    
    [closing_issues_using_keywords]: https://help.github.com/en/enterprise/2.16/user/github/managing-your-work-on-github/closing-issues-using-keywords
    
    Reviewed-on: https://codeberg.org/iliyan-kostov/nixos-config/pulls/23
    Co-authored-by: iliyan-kostov <iliyan.kostov@email.ikostov.org>
    Co-committed-by: iliyan-kostov <iliyan.kostov@email.ikostov.org>

--- hosts/shared/options/default.nix
+++ hosts/shared/options/default.nix
@@ -2,6 +2,7 @@ _: {
   imports =
     [
       ./modules
+      ./garbage-collect
       ./nixpkgs
       ./blueman
       ./env-vars
---
+++ hosts/shared/options/garbage-collect/default.nix
@@ -0,0 +1,10 @@
+_: {
+  nix = {
+    settings.auto-optimise-store = true;
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+  };
+}
