5801b914dbb1f19457a69651fbc3e7a70c090363
Iliyan Kostov <59366790+IliyanKostov9@users.noreply.github.com>
2025-02-11 18:56:34 +0000

    Feat/keyd (#134)
    * chore: removed some chromium extensions
    
    M  hosts/shared/options/ssh/known_hosts
    M  programs/system/security/firejail/default.nix
    M  programs/user/dev/shell/zsh/default.nix
    
    * feat: added whitespace
    
    M  hosts/shared/options/ssh/default.nix
    
    * feat: added whitespace
    
    M  hosts/shared/options/ssh/default.nix
    
    * feat: added ssh agent start for ikostov2
    
    M  home/ikostov2/default.nix
    A  home/ikostov2/options/ssh/default.nix
    M  hosts/shared/options/ssh/default.nix
    
    * feat: removed ssh-agent home manager
    
    M  home/ikostov2/default.nix
    D  home/ikostov2/options/ssh/default.nix
    M  hosts/shared/options/ssh/default.nix
    
    * feat: added keyd
    
    M  hosts/shared/options/modules/default.nix
    M  programs/system/default.nix
    A  programs/system/dev/default.nix
    A  programs/system/dev/keyd/default.nix
    
    * fix: keyd remaps
    
    M  programs/system/dev/keyd/default.nix
    
    * fix: tests
    
    M  home/ikostov2/default.nix
    M  programs/user/browsers/librewolf/default.nix
    
    * fix: tests
    
    M  programs/system/dev/keyd/default.nix
    M  programs/user/browsers/librewolf/default.nix
    
    * chore: changed delete key to rightshift
    
    M  programs/system/dev/keyd/default.nix

---
+++ programs/system/dev/keyd/default.nix
@@ -0,0 +1,36 @@
+{ lib, config, ... }:
+with lib;
+with lib.types;
+let cfg = config.modules.dev.keyd;
+in
+{
+  options.modules.dev.keyd = {
+    enable = mkOption {
+      type = bool;
+      default = false;
+      description = mkDoc ''
+        Enable keyd
+      '';
+    };
+  };
+
+  config = mkIf cfg.enable {
+    services.keyd = {
+      enable = true;
+      keyboards = {
+        default = {
+          ids = [ "*" ];
+          settings = {
+            main = {
+              "f1" = "left";
+              "f2" = "down";
+              "f3" = "up";
+              "f4" = "right";
+              "rightshift" = "backspace";
+            };
+          };
+        };
+      };
+    };
+  };
+}
