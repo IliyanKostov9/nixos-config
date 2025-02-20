5aa96c1495f12d78b87e1da23a5e64d3346e5c6e
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2025-02-20 14:29:54 +0200

    feat: added instructions on installing citrix
    M  programs/user/rdp/citrix/default.nix

--- programs/user/rdp/citrix/default.nix
+++ programs/user/rdp/citrix/default.nix
@@ -11,4 +11,8 @@ in
     ];
   };
 
+  # NOTE: instructions
+  # 1. download the version package:  https://www.citrix.com/downloads/workspace-app/legacy-workspace-app-for-linux/workspace-app-for-linux-latest1.html
+  # 2. then use the command: `nix-prefetch-url file://$(pwd)/linuxx64-24.5.0.76.tar.gz --type sha256 `
+
 }
