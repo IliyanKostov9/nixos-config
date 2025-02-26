f249791bbed9c698ba76ca910bd46a4012e03a30
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2025-02-26 22:32:59 +0200

    chore: removed sops
     M home/ikostov2/default.nix
     M programs/user/dev/command-line/bat/default.nix
     M programs/user/dev/command-line/default.nix
     D programs/user/dev/command-line/sops/default.nix
     M programs/user/dev/shell/common/default.nix
    M  programs/user/dev/shell/sh/alias/default.nix
     M programs/user/file-compression/p7zip/default.nix
     M programs/user/iot/qFlipper/default.nix
     M programs/user/media/vlc/default.nix
     M programs/user/office-suite/calibre/default.nix
     M programs/user/office-suite/drawio/default.nix
     M programs/user/office-suite/libreoffice/default.nix
     M programs/user/office-suite/mail/default.nix
     M programs/user/office-suite/nomacs/default.nix
     M programs/user/office-suite/okular/default.nix
     M programs/user/password-manager/keepass/default.nix

--- programs/user/dev/shell/sh/alias/default.nix
+++ programs/user/dev/shell/sh/alias/default.nix
@@ -17,7 +17,7 @@ let
     excludeShellChecks = [ "SC2005" ];
 
     text = ''
-      selection=$(find . -type f -o -type d | fzf --cycle --border=thinblock --border-label='| Search here |' --preview 'bat --color=always --style=numbers --theme=base16-256 --line-range=:500 {} || tree -C {}' --preview-label='Preview');
+      selection=$(find . -type f -o -type d | fzf --cycle --border=thinblock --border-label='| Search here |' --preview 'bat --line-range=:500 {} || tree -C {}' --preview-label='Preview');
 
       if [ -d "$selection" ]; then
         echo "$selection" 
