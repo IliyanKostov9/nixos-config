c0fa2195500aba6cd40a7b3d7a3be19a2eb3abc6
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2025-03-01 11:29:45 +0200

    feat: removed sys docker
    M  hosts/shared/options/virtualisation/default.nix

--- hosts/shared/options/virtualisation/default.nix
+++ hosts/shared/options/virtualisation/default.nix
@@ -1,24 +1,10 @@
 { pkgs, ... }:
 {
   virtualisation = {
+    spiceUSBRedirection.enable = true;
     libvirtd = {
       enable = true;
       qemu.vhostUserPackages = with pkgs; [ virtiofsd ];
-    };
-    spiceUSBRedirection.enable = true;
-    docker = {
-      enable = true;
-      rootless = {
-        enable = true;
-        setSocketVariable = true;
-      };
-      daemon.settings = {
-        userland-proxy = false;
-        experimental = true;
-        metrics-addr = "0.0.0.0:9323";
-        ipv6 = true;
-        fixed-cidr-v6 = "fd00::/80";
-      };
     };
   };
 }
