aa0c448980a69f3ab30465060321832234b87124
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2025-03-08 19:57:48 +0200

    feat: added devenv
    A  .envrc
    M  .gitignore
    M  flake.lock
    M  flake.nix
    M  flakes/dev-shell.nix

--- flake.nix
+++ flake.nix
@@ -2,10 +2,17 @@
   description = "Iliyan K's NixOS config";
 
   inputs = {
+    devenv-root = {
+      url = "file+file:///dev/null";
+      flake = false;
+    };
+    devenv.url = "github:cachix/devenv";
+
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nur.url = "github:nix-community/NUR";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -32,17 +39,50 @@
     # };
   };
 
-  outputs = {flake-parts, ...} @ inputs:
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = {
+    flake-parts,
+    devenv,
+    devenv-root,
+    ...
+  } @ inputs:
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux" "aarch64-linux"];
 
       imports = [
         inputs.flake-parts.flakeModules.easyOverlay
+        inputs.devenv.flakeModule
         # ./flakes/dev-shell.nix
         ./flakes/system.nix
         ./flakes/user.nix
         # ./flakes/export-image.nix
         ./tests
       ];
+
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
+        devenv.shells.default = {
+          name = "NixOS devenv";
+          git-hooks.hooks = {
+            # Common
+            actionlint.enable = true;
+            checkmake.enable = true;
+          };
+          devenv.root = let
+            devenvRootFileContent = builtins.readFile inputs.devenv-root.outPath;
+          in
+            pkgs.lib.mkIf (devenvRootFileContent != "") devenvRootFileContent;
+        };
+      };
     };
 }
--- flakes/dev-shell.nix
+++ flakes/dev-shell.nix
@@ -1,22 +1,22 @@
-_: {
-  flake.perSystem = { inputs', pkgs, ... }: {
+{inputs, ...}: {
+  flake.perSystem = {
+    inputs,
+    pkgs,
+    ...
+  }: {
     flake.devenv.shells.default = {
       name = "Nix devenv";
-      languages.ansible.enable = true;
       git-hooks.hooks = {
         # Common
         markdownlint.enable = true;
         actionlint.enable = true;
         checkmake.enable = true;
-        prettier.enable = true;
-
         # Nix
         statix.enable = true;
       };
-      flake.devenv.root =
-        let
-          devenvRootFileContent = builtins.readFile inputs'.devenv-root.outPath;
-        in
+      flake.devenv.root = let
+        devenvRootFileContent = builtins.readFile inputs.devenv-root.outPath;
+      in
         pkgs.lib.mkIf (devenvRootFileContent != "") devenvRootFileContent;
     };
   };
