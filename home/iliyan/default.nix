99752fba7f72b5db3292655d1ca43062f5673e0e
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2025-09-25 21:10:44 +0200

    feat: chromium
    M  home/iliyan/default.nix

--- home/iliyan/default.nix
+++ home/iliyan/default.nix
@@ -75,13 +75,10 @@ in {
     api.postman.enable = true;
     #rdp.citrix.enable = true;
 
-    browsers = {
-      librewolf = {
-        package = null; # NOTE: For firejail
-        enable = false;
-        profiles = import ./options/librewolf/profiles {inherit pkgs;};
-      };
-      chromium.enable = true;
+    browsers.librewolf = {
+      package = null; # NOTE: For firejail
+      enable = false;
+      profiles = import ./options/librewolf/profiles {inherit pkgs;};
     };
 
     dev = {
