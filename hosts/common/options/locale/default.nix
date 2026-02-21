2c066f552a3e090cb53ba8f7fdf2d712c771b3ef
Iliyan Kostov <59366790+IliyanKostov9@users.noreply.github.com>
2026-02-21 13:56:44 +0100

    Chore/change sys lang to fr (#254)
    * feat: added serial port group
    
    M  config.nix
    
    * feat: changed language to french
    
    M  hosts/common/options/locale/default.nix
    
    feat: changed language to french
    
    M  hosts/common/options/locale/default.nix
    M  hosts/common/options/modules/default.nix

--- hosts/common/options/locale/default.nix
+++ hosts/common/options/locale/default.nix
@@ -3,11 +3,10 @@
   config,
   ...
 }:
- with lib; let
+with lib; let
   cfg = config.modules.options.locale;
 in {
   options.modules.options.locale = {
-
     timeZone = mkOption {
       type = types.str;
       default = "Europe/Paris";
@@ -25,30 +24,29 @@ in {
       description = mkDoc ''
         Choose the default locale.
         Choices:
-        - en_US.UTF-8
-        - den_US.UTF-8e_DE.UTF-8
-        - en_US.UTF-8
         - bg_BG.UTF-8
+        - en_US.UTF-8
+        - fr_FR.UTF-8
+        - den_US.UTF-8e_DE.UTF-8
       '';
     };
-
   };
 
   config = {
-  time.timeZone = cfg.timeZone;
-  i18n = {
-    inherit (cfg) defaultLocale;
-    extraLocaleSettings = {
-      LC_ADDRESS = cfg.defaultLocale;
-      LC_IDENTIFICATION = cfg.defaultLocale;
-      LC_MEASUREMENT = cfg.defaultLocale;
-      LC_MONETARY = cfg.defaultLocale;
-      LC_NAME = cfg.defaultLocale;
-      LC_NUMERIC = cfg.defaultLocale;
-      LC_PAPER = cfg.defaultLocale;
-      LC_TELEPHONE = cfg.defaultLocale;
-      LC_TIME = cfg.defaultLocale;
+    time.timeZone = cfg.timeZone;
+    i18n = {
+      inherit (cfg) defaultLocale;
+      extraLocaleSettings = {
+        LC_ADDRESS = cfg.defaultLocale;
+        LC_IDENTIFICATION = cfg.defaultLocale;
+        LC_MEASUREMENT = cfg.defaultLocale;
+        LC_MONETARY = cfg.defaultLocale;
+        LC_NAME = cfg.defaultLocale;
+        LC_NUMERIC = cfg.defaultLocale;
+        LC_PAPER = cfg.defaultLocale;
+        LC_TELEPHONE = cfg.defaultLocale;
+        LC_TIME = cfg.defaultLocale;
+      };
     };
-  };
   };
 }
