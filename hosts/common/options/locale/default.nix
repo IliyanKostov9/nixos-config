253e693e2bab3f70a49028ecf3a7030ab97c1d53
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2026-02-21 13:45:00 +0100

    feat: changed language to french
    M  hosts/common/options/locale/default.nix

--- hosts/common/options/locale/default.nix
+++ hosts/common/options/locale/default.nix
@@ -3,11 +3,10 @@
   config,
   ...
 }:
- with lib; let
+with lib; let
   cfg = config.modules.options.locale;
 in {
   options.modules.options.locale = {
-
     timeZone = mkOption {
       type = types.str;
       default = "Europe/Paris";
@@ -21,7 +20,7 @@ in {
 
     defaultLocale = mkOption {
       type = types.str;
-      default = "en_US.UTF-8";
+      default = "fr_FR.UTF-8";
       description = mkDoc ''
         Choose the default locale.
         Choices:
@@ -31,24 +30,23 @@ in {
         - bg_BG.UTF-8
       '';
     };
-
   };
 
   config = {
-  time.timeZone = cfg.timeZone;
-  i18n = {
-    inherit (cfg) defaultLocale;
-    extraLocaleSettings = {
-      LC_ADDRESS = cfg.defaultLocale;
-      LC_IDENTIFICATION = cfg.defaultLocale;
-      LC_MEASUREMENT = cfg.defaultLocale;
-      LC_MONETARY = cfg.defaultLocale;
-      LC_NAME = cfg.defaultLocale;
-      LC_NUMERIC = cfg.defaultLocale;
-      LC_PAPER = cfg.defaultLocale;
-      LC_TELEPHONE = cfg.defaultLocale;
-      LC_TIME = cfg.defaultLocale;
+    time.timeZone = cfg.timeZone;
+    i18n = {
+      inherit (cfg) defaultLocale;
+      extraLocaleSettings = {
+        LC_ADDRESS = cfg.defaultLocale;
+        LC_IDENTIFICATION = cfg.defaultLocale;
+        LC_MEASUREMENT = cfg.defaultLocale;
+        LC_MONETARY = cfg.defaultLocale;
+        LC_NAME = cfg.defaultLocale;
+        LC_NUMERIC = cfg.defaultLocale;
+        LC_PAPER = cfg.defaultLocale;
+        LC_TELEPHONE = cfg.defaultLocale;
+        LC_TIME = cfg.defaultLocale;
+      };
     };
-  };
   };
 }
