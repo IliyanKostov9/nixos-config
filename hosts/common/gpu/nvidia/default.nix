07542897c19061686e2f54c308583691de4a0617
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2026-07-13 22:36:38 +0300

    fix: nvidia
    M  hosts/common/gpu/nvidia/default.nix
    M  hosts/lenovo-legion-16ahp9/default.nix

--- hosts/common/gpu/nvidia/default.nix
+++ hosts/common/gpu/nvidia/default.nix
@@ -22,15 +22,14 @@
       modesetting.enable = true;
       nvidiaSettings = true;
       # package = config.boot.kernelPackages.nvidiaPackages.beta;
-      # package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-      #   version = "595.45.04";
-      #   sha256_64bit = "sha256-zUllSSRsuio7dSkcbBTuxF+dN12d6jEPE0WgGvVOj14=";
-      #   sha256_aarch64 = "sha256-jl6lQWsgF6ya22sAhYPpERJ9r+wjnWzbGnINDpUMzsk=";
-      #   openSha256 = "sha256-uqNfImwTKhK8gncUdP1TPp0D6Gog4MSeIJMZQiJWDoE=";
-      #   settingsSha256 = "sha256-Y45pryyM+6ZTJyRaRF3LMKaiIWxB5gF5gGEEcQVr9nA=";
-      #   persistencedSha256 = "sha256-5FoeUaRRMBIPEWGy4Uo0Aho39KXmjzQsuAD9m/XkNpA=";
-      #   usePersistenced = false;
-      # };
+      package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+        version = "610.43.02";
+        sha256_64bit = "sha256-MDSgVLtM33dS/43CclZMsQVROAS/9TU4lFkBsWyndGM=";
+        openSha256 = "sha256-hP5NVZZ4vGsACHLmUDKq4uckpd/kn1GxCSYnnJfAuBs=";
+        settingsSha256 = "sha256-0YAhufRgjDW+uR+kjaTb154fibpcDw8QowfrucoZsKE=";
+        persistencedSha256 = "sha256-Whgv9X+v2fRhzliOl2LzltY9v1SxDafFfv3IUPqj/hk=";
+        usePersistenced = false;
+      };
     };
     nvidia-container-toolkit.enable = true;
   };
