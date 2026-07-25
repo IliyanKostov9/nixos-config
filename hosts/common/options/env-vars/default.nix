fa96e8da4375ef29625cc30c4ad3fdc1822c54ef
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2026-07-25 12:21:53 +0300

    feat: added env var
    M  hosts/common/options/env-vars/default.nix

--- hosts/common/options/env-vars/default.nix
+++ hosts/common/options/env-vars/default.nix
@@ -4,6 +4,7 @@
       NIXOS_OZONE_HL = "1";
       GBM_BACKEND = "nvidia-drm";
       __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+      ELECTRON_OZONE_PLATFORM_HINT = "auto";
     }
     // host_attr.variables;
 }
