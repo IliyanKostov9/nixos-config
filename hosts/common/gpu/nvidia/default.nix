e675b404218a1e8e625f61f0b2d04d1ca043e2df
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2026-08-08 17:54:04 +0300

    chore: refactored the nvidia module
    M  hosts/common/gpu/nvidia/default.nix
    M  hosts/common/gpu/nvidia/laptop/prime/default.nix
    M  hosts/common/options/modules/default.nix

--- hosts/common/gpu/nvidia/default.nix
+++ hosts/common/gpu/nvidia/default.nix
@@ -1,34 +1,125 @@
 {
+  lib,
   config,
   pkgs,
   ...
-}: {
-  services.xserver.videoDrivers = [
-    "nvidia"
-  ];
-
-  hardware = {
-    graphics = {
-      enable = true;
-      enable32Bit = true;
-      extraPackages = with pkgs; [
-        nvidia-vaapi-driver
-      ];
+}:
+with lib; let
+  cfg = config.modules.nvidia;
+  availableChannels = ["stable" "beta"];
+in {
+  options.modules.nvidia = {
+    openSource = mkOption {
+      type = types.bool;
+      default = true;
+      description = mkDoc ''
+        Should I use the open source Nvidia's drivers?
+      '';
     };
 
-    nvidia-container-toolkit.enable = true;
-    nvidia = {
-      open = false;
-      modesetting.enable = true;
-      nvidiaSettings = true;
-      # package = config.boot.kernelPackages.nvidiaPackages.stable;
-      package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-        version = "610.57.04";
-        sha256_64bit = "sha256-suk1xmuDuwDAyFe8jg7g/VLekoa0DJzB7sKafOfrEW0=";
-        sha256_aarch64 = "sha256-QCefrMBCmpOwuOyXv1k5Gj0iB2CYlPgnG3JToUw/j54=";
-        openSha256 = "sha256-rQHOOOY4KL92Ww3KDwh+j4eGU7oNAH8LutZC5wmFnPo=";
-        settingsSha256 = "sha256-ZEMo8I8Zc2Tq6RVDNYpAH+f094dUaZiBqO+5f6lIjRI=";
-        persistencedSha256 = "sha256-aXmD2VY1RLlgAnlHhOUMWzvMyhI6JTClcFLm4imF/mA=";
+    channel = mkOption {
+      type = types.str;
+      default = "stable";
+      description = mkDoc ''
+        Nvidia's channel to use
+      '';
+    };
+
+    version = mkOption {
+      type = types.str;
+      default = "";
+      description = mkDoc ''
+        Version of Nvidia's driver (if channel is not specified)
+      '';
+    };
+
+    sha256_64bit = mkOption {
+      type = types.str;
+      default = "";
+      description = mkDoc ''
+        SHA256 Version of Nvidia's driver (if channel is not specified)
+      '';
+    };
+
+    sha256_aarch64 = mkOption {
+      type = types.str;
+      default = "";
+      description = mkDoc ''
+        SHA256 Version of Nvidia's driver (if channel is not specified)
+      '';
+    };
+
+    openSha256 = mkOption {
+      type = types.str;
+      default = "";
+      description = mkDoc ''
+        SHA256 Version of Nvidia's driver (if channel is not specified)
+      '';
+    };
+
+    settingsSha256 = mkOption {
+      type = types.str;
+      default = "";
+      description = mkDoc ''
+        SHA256 Version of Nvidia's driver (if channel is not specified)
+      '';
+    };
+
+    persistencedSha256 = mkOption {
+      type = types.str;
+      default = "";
+      description = mkDoc ''
+        SHA256 Version of Nvidia's driver (if channel is not specified)
+      '';
+    };
+  };
+
+  config = {
+    assertions = [
+      {
+        assertion = (cfg.channel != "" && cfg.version == "") || (cfg.channel == "" && cfg.version != "");
+        message = ''
+          Both channel and version should not be used at the same time!
+        '';
+      }
+      {
+        assertion = cfg.channel == "" || builtins.elem cfg.channel availableChannels;
+        message = ''
+          Channel ${cfg.channel} is incorrect. Availible channels: ${lib.concatStringsSep '', '' availableChannels}
+        '';
+      }
+      {
+        assertion = cfg.version != "" && cfg.sha256_64bit != "" && cfg.sha256_aarch64 != "" && cfg.openSha256 != "" && cfg.settingsSha256 != "" && cfg.persistencedSha256 != "";
+        message = ''
+          The Version ${cfg.version} should include sha256 hashes to pin it!
+        '';
+      }
+    ];
+    services.xserver.videoDrivers = [
+      "nvidia"
+    ];
+
+    hardware = {
+      graphics = {
+        enable = true;
+        enable32Bit = true;
+        extraPackages = with pkgs; [
+          nvidia-vaapi-driver
+        ];
+      };
+
+      nvidia-container-toolkit.enable = true;
+      nvidia = {
+        open = cfg.openSource;
+        modesetting.enable = true;
+        nvidiaSettings = true;
+        package =
+          if cfg.channel != ""
+          then config.boot.kernelPackages.nvidiaPackages.${cfg.channel}
+          else
+            config.boot.kernelPackages.nvidiaPackages.mkDriver {
+              inherit (cfg) version sha256_64bit sha256_aarch64 openSha256 settingsSha256 persistencedSha256;
+            };
       };
     };
   };
--- hosts/common/gpu/nvidia/laptop/prime/default.nix
+++ hosts/common/gpu/nvidia/laptop/prime/default.nix
@@ -6,12 +6,16 @@
 with lib; let
   cfg = config.modules.nvidia.prime;
 in {
+  imports = [
+    ../../default.nix
+  ];
+
   options.modules.nvidia.prime = {
     enableSync = mkOption {
       type = types.bool;
       default = true;
       description = mkDoc ''
-        Enable sync mode to offload to Nvidia's GPU 
+        Enable sync mode to offload to Nvidia's GPU
       '';
     };
 
@@ -19,14 +23,19 @@ in {
       type = types.bool;
       default = false;
       description = mkDoc ''
-        Enable sync mode to offload to AMD's GPU 
+        Enable sync mode to offload to AMD's GPU
       '';
     };
   };
 
   config = {
-    imports = [
-      ../default.nix
+    assertions = [
+      {
+        assertion = cfg.enableSync != cfg.enableOffload;
+        message = ''
+          You cannot have both enableSync and enableOffload both set to ${boolToString cfg.enableSync}
+        '';
+      }
     ];
 
     hardware.nvidia.prime = {
@@ -34,17 +43,17 @@ in {
       sync.enable = cfg.enableSync;
       offload = {
         enable = cfg.enableOffload;
-        enableOffloadCmd = cfg.enableOffload;;
+        enableOffloadCmd = cfg.enableOffload;
       };
     };
     specialisation = {
       on-the-go.configuration = {
         system.nixos.tags = ["on-the-go"];
         hardware.nvidia.prime = {
-          sync.enable = false;
+          sync.enable = mkForce false;
           offload = {
-            enable = true;
-            enableOffloadCmd = true;
+            enable = mkForce true;
+            enableOffloadCmd = mkForce true;
           };
         };
       };
