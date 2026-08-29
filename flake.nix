8994d6ab209a7bbd8d80b076fbf77c0828cdc8f5
Iliyan Kostov <59366790+IliyanKostov9@users.noreply.github.com>
2026-08-29 09:56:41 +0300

    chore: removed wayland pkg (#325)
    M  flake.lock
    M  flake.nix
    M  flakes/shared.nix

--- flake.nix
+++ flake.nix
@@ -5,10 +5,6 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-2511.url = "github:nixos/nixpkgs/release-25.11";
-    nixpkgs-wayland = {
-      url = "github:nix-community/nixpkgs-wayland";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
 
     devenv.url = "github:cachix/devenv";
     nur.url = "github:nix-community/NUR";
