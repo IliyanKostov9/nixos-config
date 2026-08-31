29a2688cbec7cf99e244c5ffd474ebacd7ff06df
iliyan-kostov <iliyan.kostov@email.ikostov.org>
2026-08-31 21:16:55 +0300

    feat: added amdgpu
    M  .github/dependabot.yml
    M  hosts/common/gpu/nvidia/default.nix
    M  hosts/common/options/nixpkgs/default.nix

--- hosts/common/options/nixpkgs/default.nix
+++ hosts/common/options/nixpkgs/default.nix
@@ -1,12 +1,15 @@
 {
   lib,
   stateVersion,
+  pkgs,
   ...
 }: {
   system = lib.mkDefault {
     inherit stateVersion;
   };
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  nix.settings.experimental-features = lib.mkDefault ["nix-command" "flakes" "pipe-operators"];
+  nix = {
+    package = pkgs.nixVersions.latest;
+    settings.experimental-features = lib.mkDefault ["nix-command" "flakes" "pipe-operators"];
+  };
   documentation.nixos.enable = false;
 }
